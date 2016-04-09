075b27634054dbb19261d9a39a217e74219d1a28
Pawan <pwanster11@gmail.com>
2016-04-09 00:04:44 -0400

    modified JS file to include clickability

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
   var input;
-  
-  /*$("#searchbutton").click(function() 
+
+  $("#searchbutton").click(function() 
     { getWiki(
-      $("#inputsearch").val()); 
-      
-    
-    });*/
+      $("#inputsearch").val());
+
+
+    });
   $(".search-query").keypress(function(event) {
        if(event.which === 13) {
            input = $("#inputsearch").val();
-           
+
            event.preventDefault();
            getWiki(input);
-       } 
+       }
     });
 
-    
+
 });
 
-function getWiki(input) {  
+function getWiki(input) {
   var wiki ="http://en.wikipedia.org//w/api.php?action=opensearch&format=json&search="+input+ "&limit=12&callback=?";
     $.ajax({
         dataType: "jsonp",
@@ -30,12 +30,12 @@ function getWiki(input) {
 
 function callback(data){
     //console.log(data);
-  $("ul").html("");  
+  $("ul").html("");
   for (var i = 0; i < data[1].length; i++) {
     var entry = '<li><h4><a href="' + data[3][i] + '"target="_blank">' + data[1][i] + '</a></h4><span>' + data[2][i] +'</span>';
     $("ul").append(entry);
     $(".result").css('background', 'rgba(300,300,300, .8)');
-    
+
 }
-  
+
 }
